0e31906c4b225ae77755d895c1974a5de6a7589b
Jean Furman <jean_11spfc@hotmail.com>
2023-09-02 18:37:09 -0300

    Adicionando busca por cartorios associados

--- src/main/java/com/escriba/cartorio/repository/AtribuicaoRepository.java
+++ src/main/java/com/escriba/cartorio/repository/AtribuicaoRepository.java
@@ -1,13 +1,21 @@
 package com.escriba.cartorio.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.escriba.cartorio.model.Atribuicao;
+import com.escriba.cartorio.model.Cartorio;
 
 public interface AtribuicaoRepository extends JpaRepository<Atribuicao, String> {
 	
 	Optional<Atribuicao> findByNome(String name);
+	
+	@Query("SELECT c FROM Cartorio c JOIN c.atribuicoes a WHERE a.id = :atribuicaoId")
+    List<Cartorio> findCartoriosByAtribuicaoId(@Param("atribuicaoId") String atribuicaoId);
+
 	
 }
--- src/main/java/com/escriba/cartorio/repository/SituacaoRepository.java
+++ src/main/java/com/escriba/cartorio/repository/SituacaoRepository.java
@@ -1,13 +1,20 @@
 package com.escriba.cartorio.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.escriba.cartorio.model.Cartorio;
 import com.escriba.cartorio.model.Situacao;
 
 public interface SituacaoRepository extends JpaRepository<Situacao, String> {
 	
 	Optional<Situacao> findByNome(String name);
-	
+
+	@Query("SELECT c FROM Cartorio c WHERE c.situacao.id = :situacaoId")
+    List<Cartorio> findCartoriosBySituacaoId(@Param("situacaoId") String situacaoId);
+
 }
